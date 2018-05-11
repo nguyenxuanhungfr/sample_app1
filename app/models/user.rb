735d79a47d3915ea353d39be1c1b5012d34bfe31
hungxuan <nguyen.xuan.hung@framgia.com>
2018-05-11 14:33:56 +0700

    chap12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save  :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: {maximum: Settings.user_model.max_length_name}
@@ -57,14 +57,27 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
-  class << self
-    def digest string
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  private
+    def downcase_email
+      self.email = email.downcase
     end
 
-    def new_token
-      SecureRandom.urlsafe_base64
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
-  end
 end
